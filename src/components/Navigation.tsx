276e396792647412e09b892190e193436cecadb5
yutatakabatake <yutatakabatake@gmail.com>
2026-03-07 18:22:38 +0900

    Make basic navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,25 +4,26 @@ import Calender from '../pages/Calender';
 import Project from '../pages/Project';
 import { ListTodo, Calendar, Folder } from 'lucide-react';
 
-
 function Navigation() {
     return (
-        <>
-            <BrowserRouter>
+        <BrowserRouter>
+            <header className='border-b px-6 py-4 sticky top-0 z-0'>
                 <nav>
-                    <h1>TODO APP</h1>
-                    <Link to="/"><ListTodo />Tasks</Link>
-                    <Link to="/calender"><Calendar />Calender</Link>
-                    <Link to="/project"><Folder />Project</Link>
+                    <h1 className='font-bold text-2xl mb-4'>TODO APP</h1>
+                    <div className='flex gap-6'>
+                        <Link to="/" className='flex bg-blue-400 gap-1 px-4 py-2 rounded-xl'><ListTodo />Tasks</Link>
+                        <Link to="/calender" className='flex'><Calendar />Calender</Link>
+                        <Link to="/project" className='flex'><Folder />Project</Link>
+                    </div>
                 </nav>
+            </header>
 
-                <Routes>
-                    <Route path='/' element={<TaskList />} />
-                    <Route path='/calender' element={<Calender />} />
-                    <Route path='/project' element={<Project />} />
-                </Routes>
-            </BrowserRouter >
-        </>
+            <Routes>
+                <Route path='/' element={<TaskList />} />
+                <Route path='/calender' element={<Calender />} />
+                <Route path='/project' element={<Project />} />
+            </Routes>
+        </BrowserRouter >
     )
 }
 
