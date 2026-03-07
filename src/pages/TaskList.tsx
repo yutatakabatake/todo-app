b4eb3fdb00622f1b0e4bdc0b98e8420447a23245
yutatakabatake <yutatakabatake@gmail.com>
2026-03-07 22:50:26 +0900

    Add tasklist page top

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,6 +1,20 @@
+import ButtonUsage from "../components/Button"
+
 function TaskList() {
     return (
-        <div>TaskList</div>
+        <>
+            <div className="border-b px-6 py-4">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="text-xl font-semibold">TaskList</h2>
+                        <p className="text-gray-600">X pending tasks</p>
+                    </div>
+                    <div>
+                        <ButtonUsage handleClick={() => alert('click!')} text='new' color="success" />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
