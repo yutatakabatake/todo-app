bb4d9aa49b541d33be4a9eeb9f38b249255db9fe
yutatakabatake <yutatakabatake@gmail.com>
2026-03-21 19:51:27 +0900

    Fix SQL to match the front-end properties

--- back_end/src/services/taskService.js
+++ back_end/src/services/taskService.js
@@ -1,6 +1,19 @@
 import query from "../db.js";
 
 export async function getAllTasks() {
-    const { rows } = await query('SELECT id , title , project_id , done , task_date , expected_time , start_time , actual_time , time_slot , is_working FROM tasks_tb');
+    const { rows } = await query(`
+        SELECT 
+            id, 
+            title, 
+            project_id AS "projectId", 
+            done, 
+            TO_CHAR(task_date, 'YYYY/MM/DD') AS "date",
+            expected_time AS "expectedTime", 
+            start_time AS "startTime", 
+            actual_time AS "actualTime", 
+            time_slot AS "timeSlot", 
+            is_working AS "isWorking"
+        FROM tasks_tb 
+        ORDER BY task_date ASC`);
     return rows;
 }
