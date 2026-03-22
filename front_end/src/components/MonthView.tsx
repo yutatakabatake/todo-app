8b3446fb5e90b55159d47fa09267e700bd82f1de
yutatakabatake <yutatakabatake@gmail.com>
2026-03-22 23:02:14 +0900

    Add week days

--- front_end/src/components/MonthView.tsx
+++ front_end/src/components/MonthView.tsx
@@ -1,3 +1,5 @@
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 function MonthView() {
     return (
         <div className="flex flex-col h-full">
@@ -10,6 +12,16 @@ function MonthView() {
 
             <div className="flex-1 overflow-y-auto p-4">
                 <div className="overflow-hidden">
+                    <div className="grid grid-cols-7 gap-2 mb-2">
+                        {weekDays.map((day, index) => (
+                            <div
+                                key={index}
+                                className={`text-center font-medium py-2 ${index === 0 ? 'text-red-600' : index === 6 ? 'text-blue-600' : 'text-gray-700'
+                                    }`}>
+                                {day}
+                            </div>
+                        ))}
+                    </div>
 
                 </div>
             </div>
