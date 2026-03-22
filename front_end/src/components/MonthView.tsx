e14a3f1c2bdf28234c9b48b173d466089f85e229
yutatakabatake <yutatakabatake@gmail.com>
2026-03-22 22:54:21 +0900

    Modify size of calender page top

--- front_end/src/components/MonthView.tsx
+++ front_end/src/components/MonthView.tsx
@@ -1,7 +1,18 @@
 function MonthView() {
     return (
-        <div className="overflow-hidden">
-            <div>MonthView</div>
+        <div className="flex flex-col h-full">
+            <div className="bg-white z-10 border-b px-6 py-2 sticky top-0">
+                <h3 className="text-xl font-semibold">
+                    2026 March
+                </h3>
+                <p className="text-sm text-gray-500 mt-1">Done tasks</p>
+            </div>
+
+            <div className="flex-1 overflow-y-auto p-4">
+                <div className="overflow-hidden">
+
+                </div>
+            </div>
         </div>
     )
 }
