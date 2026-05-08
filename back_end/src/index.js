57c0a89340b44be8b8bfa096e39fe1914e808f7d
yutatakabatake <yutatakabatake@gmail.com>
2026-05-09 00:17:53 +0900

    Add environment variables

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 import taskRouter from './routes/taskRoute.js';
 import projectRouter from './routes/projectRoute.js';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
 app.use('/api', taskRouter);
 app.use('/api', projectRouter)
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
 })
